0bbc2e679573bcc1a19a4ecb70dbe0cfdf1794b2
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2023-06-15 12:14:41 +0100

    updated url

--- delta/urls.py
+++ delta/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('results/<int:pk>/', views.DisplayPollingUnitResult.as_view(), name='results'),
-    path('results/<int:pk>/total', views.DisplayTotalPollingUnitResult, name='total'),
+    path('total/', views.DisplayTotalPollingUnitResult, name='total'),
     path('create/', views.StorePollingUnitResult, name='create'),
 ]
