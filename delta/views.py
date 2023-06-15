943119e035da47f7d161d7e5eb6517f7779b0580
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2023-06-15 12:37:09 +0100

    [feat]: add docs and fix syntax

--- delta/views.py
+++ delta/views.py
@@ -1,32 +1,33 @@
 from typing import Any
-from django.http import HttpRequest, JsonResponse, HttpResponse
+
+import requests
+from django.db.models import Sum
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
 from django.views import generic
-from django.db.models import Sum
-
-from rest_framework.views import APIView
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 
-from delta.serializers import PollingUnitResultSerializer
 from delta.forms import PollingUnitResultSerializer
+from delta.models import AnnouncedPuResults, Lga, PollingUnit
+from delta.serializers import PollingUnitResultSerializer
 
 # from django.db.models import RawSQL
 
 
-import requests
-
-from delta.models import AnnouncedPuResults, PollingUnit, Lga
-
-
 class DisplayPollingUnitResult(APIView):
+    """
+    Pass in the polling unit unique id to the url
+    """
+
     renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'polling_unit_result.html'
+    template_name = "polling_unit_result.html"
 
     def get(self, request, pk, *args, **kwargs):
         queryset = AnnouncedPuResults.objects.filter(polling_unit_uniqueid=pk)
-        return Response({'results': queryset})
+        return Response({"results": queryset})
 
 
 def StorePollingUnitResult(request):
@@ -35,21 +36,26 @@ def StorePollingUnitResult(request):
         if form.is_valid():
             form.save(commit=False)
             form.save()
-            return HttpResponse('Result added successfully')
+            return HttpResponse("Result added successfully")
     else:
         form = PollingUnitResultSerializer()
-    return render(request, 'store_result.html', {'form': form})
-
+    return render(request, "store_result.html", {"form": form})
 
 
 def DisplayTotalPollingUnitResult(request):
-    if request.method == 'POST':
-        selected_item = request.POST.get('selected_item')
+    """
+    query polling unit table with lga_id, pick the uniqueid
+    then use the uniqueid to query announced polling unit table,
+    then use a dictionary to remove duplicate,
+    then sum the result
+    """
+    if request.method == "POST":
+        selected_item = request.POST.get("selected_item")
         data = PollingUnit.objects.filter(lga_id=selected_item)
     else:
         data = PollingUnit.objects.all()
 
-    items = Lga.objects.values_list('lga_id', flat=True)  # Get all item names
+    items = Lga.objects.values_list("lga_id", flat=True)  # Get all item names
 
     poll_list = []
     for p in data:
@@ -70,8 +76,8 @@ def DisplayTotalPollingUnitResult(request):
     total = sum(res_dict.values())
 
     context = {
-        'items': items,
-        'data': res_dict,
-        'total': total,
+        "items": items,
+        "data": res_dict,
+        "total": total,
     }
-    return render(request, 'total_polling_unit_result.html', context)
+    return render(request, "total_polling_unit_result.html", context)
